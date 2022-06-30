6692679c7367b643ea3324f4592f383197c64e39
Xander Hirsch <xh@xanderhirsch.us>
2022-06-29 23:51:03 -0700

    Nvim plugin setup

--- config/nvim/init.vim
+++ config/nvim/init.vim
@@ -5,3 +5,5 @@ set tabstop=4
 set softtabstop=4
 set shiftwidth=4
 set noexpandtab
+
+lua require('plugins')
--- fetch-git-prompt.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-URL='https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh'
-curl $URL | sed 's/PS1=/RPS1=/' && echo "__git_ps1 '' ''"
