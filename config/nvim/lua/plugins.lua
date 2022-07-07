6c1afd4a8ac8d5b08284749e4e2875a57af4c6db
Xander Hirsch <xh@xanderhirsch.us>
2022-07-07 16:32:13 -0700

    Add Neovim LSP plugin

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,5 +1,6 @@
 return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
+  use 'neovim/nvim-lspconfig'
   use 'airblade/vim-gitgutter'
   use 'altercation/vim-colors-solarized'
   use 'LnL7/vim-nix'
