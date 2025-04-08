c913bbb98eca10486988439b1277de1e182a6df8
Xander Hirsch <xander@xanderhirsch.net>
2025-04-08 12:25:16 -0500

    Run plugin updates only every 24 hours

--- .chezmoiscripts/run_after_81-update-plugins.sh
+++ .chezmoiscripts/run_after_81-update-plugins.sh
@@ -1,10 +1,31 @@
 #!/usr/bin/env bash
 
-echo 'Updating Tmux plugins...'
-$HOME/.tmux/plugins/tpm/bin/update_plugins all > /dev/null
+LAST_UPDATE_FILE=$HOME/.plugins-last-update
 
-echo 'Updating Vim plugins...'
-vim -Ec 'PlugUpdate|qa' > /dev/null
+if [[ -f $LAST_UPDATE_FILE ]]; then
+    LAST_UPDATE_DIFFERENCE=$(($(date +%s) - $(date -r $LAST_UPDATE_FILE +%s)))
 
-echo 'Updating Zsh plugins...'
-zsh -c 'source "${ZDOTDIR}/.zplug/init.zsh" && zplug update' > /dev/null
+    if [[ $LAST_UPDATE_DIFFERENCE -lt 86400 ]]; then
+        echo 'Plugins updated in the past 24 hours. Skipping.'
+        exit 0
+    fi
+fi
+touch $LAST_UPDATE_FILE
+
+if which tmux > /dev/null
+then
+    echo 'Updating Tmux plugins...'
+    $HOME/.tmux/plugins/tpm/bin/update_plugins all > /dev/null
+fi
+
+if which vim > /dev/null
+then
+    echo 'Updating Vim plugins...'
+    vim -Ec 'PlugUpdate|qa' > /dev/null
+fi
+
+if which zsh > /dev/null
+then
+    echo 'Updating Zsh plugins...'
+    zsh -c 'source "${ZDOTDIR}/.zplug/init.zsh" && zplug update' > /dev/null
+fi
